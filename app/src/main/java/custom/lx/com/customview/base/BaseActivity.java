e297bc6d1e871791daf79139794e868e7e8e10f4
leitana <1130037266@qq.com>
2017-07-17 23:19:45 +0800

    第一次提交！

---
+++ app/src/main/java/custom/lx/com/customview/base/BaseActivity.java
@@ -0,0 +1,95 @@
+package custom.lx.com.customview.base;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.orhanobut.logger.Logger;
+
+import custom.lx.com.customview.R;
+
+/**
+ * 创建时间 2017/7/17
+ * Created by linxiao.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+    private LinearLayout rootLayout;
+    private TextView title;
+    private ImageView back;
+    protected final String TAG = this.getClass().getSimpleName();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_base);
+        // 经测试在代码里直接声明透明状态栏更有效
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
+
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+        if (getSupportActionBar() != null) {
+            // Enable the Up button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+        back = (ImageView) findViewById(R.id.iv_back);
+        title = (TextView) findViewById(R.id.title);
+    }
+
+    protected void setBackBtn() {
+        if (back != null) {
+            back.setVisibility(View.VISIBLE);
+            back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        }else {
+//            Logger.d(TAG).e("back is null ,please check out");
+            Logger.d("back is null ,please check out");
+        }
+
+    }
+
+    protected void setBackClickListener(View.OnClickListener l) {
+        if (back != null) {
+            back.setVisibility(View.VISIBLE);
+            back.setOnClickListener(l);
+        }else {
+//            Logger.t(TAG).e("back is null ,please check out");
+            Logger.d("back is null ,please check out");
+        }
+
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        setContentView(View.inflate(this, layoutResID, null));
+    }
+
+    @Override
+    public void setContentView(View view) {
+        rootLayout = (LinearLayout) findViewById(R.id.root_layout);
+        if (rootLayout == null) return;
+        rootLayout.addView(view, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        initToolbar();
+    }
+}
